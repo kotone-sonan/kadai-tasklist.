abb4956e76ac951ba7844b820760a878d6752e74
jiro.kirameki <ec2-user@ip-172-31-1-68.us-east-2.compute.internal>
2018-05-31 06:02:32 +0000

    Procfile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,8 +46,8 @@ class TasksController extends Controller
     {
         
          $this->validate($request, [
-            'content' => 'required|max:10',
-            'content' => 'required|max:10',
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
 
         $task = new Task;
@@ -98,7 +98,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',   // add
+            'status' => 'required|max:10',   // add
             'content' => 'required|max:191',
         ]);
         
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,19 +18,14 @@
                 @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
-        <ul>
-            @foreach ($tasks as $task)
-              <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} {{ $task->content }} </td>
-            @endforeach
-         </ul>
-    @endif
+       
            
       {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
